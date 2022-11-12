3638a12f60c741112eed336ebe48ddd8c09d32cc
Lê Ngô Anh Vũ <ngoanhmtu2002@gmail.com>
2022-11-12 23:22:29 +0700

    add customer

--- app/Http/Controllers/frontend/ProductController.php
+++ app/Http/Controllers/frontend/ProductController.php
@@ -11,6 +11,8 @@ use App\Models\Categories;
 class ProductController extends Controller
 {
     //
+
+
     public function index()
     {
         $product = Products::where('trangthai', 1)->get();
@@ -25,29 +27,5 @@ class ProductController extends Controller
             'banner' => $banner
         ];
         return view('templates.clients.product.index', $viewData);
-    }
-
-    public function detail($slug, Request $request)
-    {
-        $meta = [];
-        if ($slug) {
-            $product = Products::where('slug', $slug)->first();
-            $discount = 0;
-            $product->giaban = ($product->giaban - $discount < 0) ? 0 : $product->giaban - $discount;
-
-            if ($product) {
-                $related = Products::where('id_loaisanpham', $product->id_loaisanpham)->get();
-                $meta['title'] = $product->tensp;
-                $meta['description'] = $product->tensp;
-                $meta['url'] = $request->url();
-                $meta['image'] = asset('uploads/product/' . $product->hinhanh);
-            }
-            $viewData = [
-                'product' => $product,
-                'related' => $related,
-                'meta' => $meta,
-            ];
-        }
-        return view('templates.clients.product.detail', $viewData);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\ProductController;
 use App\Http\Controllers\Frontend\PostsController;
 use App\Http\Controllers\Categoriesontroller;
-
+use App\Http\Controllers\CustomerController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,23 +30,33 @@ Route::get('/admin', [DashboardController::class, 'show'])->name('showDashboard'
 Route::get('admin/san-pham', [ProductControllers::class, 'show'])->name('products.show');
 
 // loai tin tức trang admin
-
+// all
 Route::get('typepost', [PostsControllers::class, 'getTypePost'])->name('get.typepost'); //show all menu posts
+//add menu posts
 Route::get('create-menupost', [PostsControllers::class, 'createMenuPost'])->name('create.menupost'); //add new menu posts
 Route::post('save-menupost', [PostsControllers::class, 'saveMenuPost'])->name('save.menupost'); //save add new menu posts
+//delete
 Route::get('delete-menupost/{id}', [PostsControllers::class, 'deleteMenuPost'])->name('delete.menupost'); //delete menu posts
+//edit
 Route::get('edit-menupost/{id}', [PostsControllers::class, 'editMenuPost'])->name('edit.menupost'); //edit menu posts
 Route::post('save-edit-menupost/{id}', [PostsControllers::class, 'saveeditMenuPost'])->name('save.edit.menupost'); //save edit menu posts
+//active
 Route::get('active-menupost/{id}', [PostsControllers::class, 'activeMenuPost'])->name('active.menupost'); //active menu posts
 
 // Tin tức trang admin:
+// all
 Route::get('post', [PostsControllers::class, 'getPost'])->name('get.post'); //show all news post
+//add news posts:
 Route::get('create-post', [PostsControllers::class, 'createPost'])->name('create.post'); //add news posts
 Route::post('save-post', [PostsControllers::class, 'savePost'])->name('save.post'); //save news posts
+//delete
 Route::get('delete-post/{id}', [PostsControllers::class, 'deletePost'])->name('delete.post'); //delete new posts
+//edit
 Route::get('edit-post/{id}', [PostsControllers::class, 'editPost'])->name('edit.post'); //edit news posts
 Route::post('save-edit-post/{id}', [PostsControllers::class, 'saveeditPost'])->name('save.edit.post'); //save news posts
+//active
 Route::get('active-post/{id}', [PostsControllers::class, 'activePost'])->name('active.post'); //active and unactive news posts
+//hot
 Route::get('hot-post/{id}', [PostsControllers::class, 'hotPost'])->name('hot.post'); //hot news posts
 
 //Admin category
@@ -58,7 +68,12 @@ Route::get('admin/category-edit/{slug}', [Categoriesontroller::class, 'edit'])->
 Route::post('admin/category-edit/{id}', [Categoriesontroller::class, 'update'])->name('categories.edithandle');
 Route::get('active-category/{id}', [Categoriesontroller::class, 'activeCategory'])->name('active.category');
 
-// Admin Products
+//trang chủ
+Route::get('/', [HomeController::class, 'index'])->name('get.home');
+
+//Route::post('/quickview', 'HomeController@quickView')->name('quickview');
+
+//products
 Route::get('products',  [ProductController::class, 'index'])->name('product');
 Route::get('admin/them-san-pham', [ProductControllers::class, 'addProductView'])->name('products.addview');
 Route::post('admin/them-san-pham', [ProductControllers::class, 'addProductHandle'])->name('products.addhandle');
@@ -67,14 +82,17 @@ Route::get('admin/sua-san-pham/{slug}', [ProductControllers::class, 'editProduct
 Route::post('admin/sua-san-pham/{id}', [ProductControllers::class, 'updateProduct'])->name('products.edithandle');
 Route::post('/admin/cap-nhat-trang-thai', [ProductControllers::class, 'updateStatus'])->name('products.updatestatus');
 
-//trang chủ
-Route::get('/', [HomeController::class, 'index'])->name('get.home');
-
-//Route::post('/quickview', 'HomeController@quickView')->name('quickview');
-
 //tin tức
 Route::get('posts',  [PostsController::class, 'index'])->name('get.posts');
 
-//Detail Product
-Route::get('detail/{p}',  [ProductController::class, 'detail'])->name('detail');
 
+
+//danh sach tai khoan khach hang
+Route::get('/customers', [CustomerController::class,'index'])->name('show.customer');
+Route::get('/del_customers/{id}', [CustomerController::class,'delete'])->name('delete.customer');
+Route::get('/status_customers/{id}', [CustomerController::class,'updateStatus'])->name('update.status.customer');
+Route::get('/add_customers', [CustomerController::class,'add'])->name('get.add.customer');
+Route::post('/save_customers', [CustomerController::class,'saveCustomer'])->name('get.save.customer');
+Route::get('/edit_customers/{id}', [CustomerController::class,'getEditCustomer'])->name('get.edit.customer');
+Route::post('/save_edit_customers{id}', [CustomerController::class,'saveEditCustomer'])->name('save.edit.customer');
+Route::post('/sendmailcoupon', [CustomerController::class,'sendmailCustomer'])->name('sendmail.coupon');
